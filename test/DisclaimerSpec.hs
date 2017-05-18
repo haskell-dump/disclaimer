6590ecf59bbc88cd23f7fa1399cc29b30f00c75d
rbasso <rbasso@sharpgeeks.net>
2017-05-18 16:31:22 +0900

    Add a test suite

---
+++ test/DisclaimerSpec.hs
@@ -0,0 +1,18 @@
+module DisclaimerSpec where
+
+import Disclaimer
+
+import Control.Monad.Trans.Writer
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+    describe "rectArea" $ do
+      it "calculates the area of a square"    $ rectArea 5 5 `shouldBe` 25
+      it "calculates the area of a rectangle" $ rectArea 2 3 `shouldBe`  6
+
+    describe "fixLegacyNumber" $
+      it "returns positive numbers" $ property propFixLegacyPositive
+  where
+    propFixLegacyPositive x = (fst . runWriter . fixLegacyNum) x >= 0
